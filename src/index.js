5ef9e683a5e1d82a1fc0dd4b09987a53872e3237
Ian Ray <ian.ray@gethearth.com>
2022-06-11 19:54:09 -0700

    react-target

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import App from 'components/App';
 import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root');
+const container = document.getElementById('react-target');
 const root = createRoot(container);
 root.render(<App />);
